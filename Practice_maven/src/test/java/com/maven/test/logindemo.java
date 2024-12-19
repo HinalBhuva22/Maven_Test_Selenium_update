ee35904bbb4124c135c5e55ebb69623e994c7351
HinalBhuva22 <radhekrushnenterprise8@gmail.com>
2024-12-19 11:31:21 +0530

    page title practice for situational dat

--- Practice_maven/src/test/java/com/maven/test/logindemo.java
+++ Practice_maven/src/test/java/com/maven/test/logindemo.java
@@ -1,35 +1,69 @@
 package com.maven.test;
 
+import java.time.Duration;
+
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
+
 
 public class logindemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
 		
 		// WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://www.fb.com");
+		driver.get("https://zukeepr.com");
 		driver.manage().window().maximize();
 		
-		WebElement email = driver.findElement(By.id("email"));
-		WebElement pass = driver.findElement(By.id("pass"));
-		WebElement login = driver.findElement(By.name("login"));
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/div/div/header/div/div[1]/div/a")).click();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/div/div/header/div/div[2]/div[2]/a")).click();
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+	driver.findElement(By.name("user_email")).sendKeys("pmytest@yopmail.com");
+		driver.findElement(By.name("password")).sendKeys("12345");
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.xpath("//button[text()='Sign In']")).click();
 		
 		
-		email.sendKeys("test@gmail.com");
-		pass.sendKeys("pass123");
-		login.click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/div/div/header/div/div[1]/div/a[3]")).click();
 		
-		System.out.println("error printed");
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div/div/div/main/div/div[2]/div/div[2]/div[1]/div/div[1]/div/button/span/span[2]")).click();
 		
-				
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		WebElement ele = driver.findElement(By.xpath("//input[@placeholder='Enter first name']"));
+		
+		
+		ele.clear();
+		
+		
+
+	 
+		ele.sendKeys("Meera");  // Entering a new name
+		
+		driver.findElement(By.xpath("//button[span/span[text()='Save']]")).click();
+	  
+	  
+
+	
+		System.out.println("Successfully removed 'Payal' and entered the new name 'Meera'.");
+	System.out.println("successfull");
+	
+	
 
 	}
 
---
+++ Practice_maven/src/test/java/com/maven/test/page_title.java
@@ -0,0 +1,28 @@
+package com.maven.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class page_title {
+	
+	public static void main(String[] args)  {
+	
+	WebDriver driver = new ChromeDriver();
+	
+	driver.get("https://situational-data.softwarecompanyindia.com/dashboard");
+	driver.manage().window().maximize();
+
+	WebElement element = driver.findElement(By.id("root"));
+	element.click();
+	String pageTitle = driver.getTitle();
+	
+	System.out.println("Page Title: " + pageTitle);
+	
+	
+	
+	
+
+}
+}
